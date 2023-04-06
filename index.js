4a888097edceaf41a88ccce59281979d51701d3b
Julian Victorio <julianv714@gmail.com>
2023-04-06 12:17:17 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.querySelector("main").remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.id ="victory"
+
+const championName = "Julian Victorio"
+
+newHeader.innerHTML = `${championName} is the champion`
